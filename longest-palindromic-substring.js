2afd7155e09088e418c8de065d86227fe5f09c3a
James Ninnes <jamesn@hydric.fm>
2020-07-20 12:35:45 +1000

    Slight reformat.

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -1,18 +1,18 @@
-function longestPalindrome(s) {
-    let longest = '', c;
-    for (c=0; c<s.length; c++) {
-        if ((s.length-c)*2 <= longest.length) break;  // exit early if remaining can't surpass largest found
+function longestPalindrome(letters) {
+    let longest = '', i;
+    for (i = 0; i < letters.length; i++) {
+        if ((letters.length - i)*2 <= longest.length) break;  // exit early if remaining can't surpass largest found
 
-        longest = scanOutward(s, longest, c, c);   // odd length "ata"
-        longest = scanOutward(s, longest, c+1, c); // even length "atta"
+        longest = scanOutward(letters, longest, i, i);   // odd length "ata"
+        longest = scanOutward(letters, longest, i+1, i); // even length "atta"
     }
     return longest;
 }
 
-function scanOutward(s, longest, left, right) {
-    if (left > 0 && right < s.length && s[left-1] === s[right+1]) {
-        return scanOutward(s, longest, left-1, right+1);
+function scanOutward(letters, longest, left, right) {
+    if (left > 0 && right < letters.length && letters[left - 1] === letters[right + 1]) {
+        return scanOutward(letters, longest, left - 1, right + 1);
     } else {
-        return (right-left+1 >= longest.length) ? s.substring(left, right+1) : longest;
+        return (right - left + 1 >= longest.length) ? letters.substring(left, right + 1) : longest;
     }
 }
